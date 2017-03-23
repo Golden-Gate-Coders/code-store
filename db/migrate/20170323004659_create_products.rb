7840e7c3ef258cd21004ee1a6e8b99ae22e0dfbe
Danish Mughal <mughal.danish@gmail.com>
2017-03-22 18:24:58 -0700

    Added Product and Supplier model

---
+++ db/migrate/20170323004659_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.decimal :price, precision: 10, scale: 2
+      t.integer :stock
+      t.integer :supplier_id
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
