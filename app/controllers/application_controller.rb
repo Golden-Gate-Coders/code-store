276397fdec181f458fd8352c74029f795a9bcf7a
Danish Mughal <mughal.danish@gmail.com>
2017-03-23 16:07:27 -0700

    Added User model and login/logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+
+    # if session[:user_id]
+    #   @current_user = User.find_by(id: session[:user_id])
+    # else
+    #   @current_user = false
+    # end
+  end
+  helper_method :current_user
+
+  def authenticate_user!
+    redirect_to "/login" unless current_user
+  end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-
+  before_action :authenticate_user!
+  
   # RESTful routes
   def index
     # sort = params[:sort]
